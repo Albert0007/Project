d75ca38837b4f158ff6cb52ed53fa56f352ddcbf
Albert0007 <54860302+Albert0007@users.noreply.github.com>
2019-09-11 00:03:08 +0400

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,23 @@
-INSERT INTO House (id, version, address) VALUES (1, 0, 'ул.Цюрупы, 16');
+INSERT INTO organization (id, name, full_name, inn, kpp, address, phone) VALUES (1, 'ООО', 'ТАНДЕР', 12, 133, 'Исаева, 2', '252525');
 
-INSERT INTO House (id, version, address) VALUES (2, 0, 'ул.Лунина, 7');
+INSERT INTO organization (id, name, full_name, inn, kpp, address, phone) VALUES (2, 'ОАО', 'Х5', 17, 127, 'Ленина, 80', '909090');
 
-INSERT INTO Person (id, version, first_name, age) VALUES (1, 0, 'Пётр', 20);
+INSERT INTO office (id, name, address, phone, organization_id) VALUES (1, 'Магнит', 'ул.Лунина, 7', 2525251, 1);
 
-INSERT INTO Person (id, version, first_name, age) VALUES (2, 0, 'John', 25);
+INSERT INTO office (id, name, address, phone, organization_id) VALUES (2, 'Пятерочка', 'Затонная, 20', 9090901, 2);
 
-INSERT INTO Person_House (person_id, house_id) VALUES (1, 1);
+INSERT INTO employee (id, version, first_name, second_name, middle_name, position, phone, country_id, document_id,) VALUES (1, 1, 'Иван', 'Иванов', 'Сергеевич', 'Продавец', 656565, );
 
-INSERT INTO Person_House (person_id, house_id) VALUES (1, 2);
+INSERT INTO employee (id, version, first_name, second_name, middle_name, position, phone, country_id, document_id) VALUES (1, 1, 'Иван', 'Иванов', 'Сергеевич', 'Продавец', 656565,);
+
+INSERT INTO employee_office (employee_id, office_id) VALUES (1, 1);
+
+INSERT INTO employee_office (employee_id, office_id) VALUES (2, 2);
+
+INSERT INTO country (id,) VALUES (1, 2);
+
+INSERT INTO country (person_id, house_id) VALUES (1, 2);
+
+INSERT INTO document (person_id, house_id) VALUES (1, 2);
+
+INSERT INTO document (person_id, house_id) VALUES (1, 2);
