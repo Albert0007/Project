be6a2673639ed08516010c586a4be2a966d3375f
Albert0007 <54860302+Albert0007@users.noreply.github.com>
2019-09-16 17:43:24 +0400

    Update schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,96 +1,88 @@
--- Организация
-CREATE TABLE IF NOT EXISTS organization
-(
-  id        INTEGER      NOT NULL AUTO_INCREMENT COMMENT 'Уникальный идентификатор',
-  name      VARCHAR(100) NOT NULL                COMMENT 'Сокращенное название',
-  full_name VARCHAR(100) NOT NULL                COMMENT 'Полное название',
-  inn       INTEGER(50)  NOT NULL                COMMENT 'Идентификационный номер налогоплательщика',
-  kpp       INTEGER(50)  NOT NULL                COMMENT 'Код причины постановки на учет в налоговых органах',
-  address   VARCHAR(100) NOT NULL                COMMENT 'Регистрационный адрес',
-  phone     INTEGER(100)                         COMMENT 'Номер телефона',
-  is_active BOOLEAN      DEFAULT FALSE           COMMENT 'Действующая организация, если дейтвует - true',
-  version   INTEGER      NOT NULL                COMMENT 'Служебное поле hibernate',
 
-  CONSTRAINT PK_ORGANIZATION_ID PRIMARY KEY (ID)
-);
-
--- CREATE INDEX IX_ORGANIZATION_NAME  ON organization(name);
-
-
--- -------------------------------------------------------------------------------------------
--- подразделение организации
-CREATE TABLE IF NOT EXISTS office
-(
-  id                INTEGER      NOT NULL AUTO_INCREMENT COMMENT 'Уникальный идентификатор',
-  name              VARCHAR(100) NOT NULL                COMMENT 'Название',
-  address           VARCHAR(100) NOT NULL                COMMENT 'Регистрационный адрес',
-  phone             INTEGER(50)                          COMMENT 'Номер телефона',
-  is_active         BOOLEAN      DEFAULT FALSE           COMMENT 'Действующее подразделение, если дейтвует - true',
-  organization_id   INTEGER                              COMMENT 'Уникальный идентификатор организации',
-  version           INTEGER      NOT NULL                COMMENT 'Служебное поле hibernate',
-
-  CONSTRAINT PK_OFFICE_ID PRIMARY KEY (id)
-);
-
-
--- --------------------------------------------------------------------------------------------------
--- сотрудник
-CREATE TABLE IF NOT EXISTS employee
-(
-  id                 INTEGER      NOT NULL AUTO_INCREMENT COMMENT 'Уникальный идентификатор',
-  first_name         VARCHAR(100) NOT NULL                COMMENT 'Имя',
-  second_name        VARCHAR(100)                         COMMENT 'Фамилия',
-  middle_name        VARCHAR(100)                         COMMENT 'Отчество',
-  position           VARCHAR(100) NOT NULL                COMMENT 'Должность',
-  phone              INTEGER(50)                          COMMENT 'Телефон',
-  is_identified      BOOLEAN      DEFAULT FALSE           COMMENT 'Работающий или не работающий, если работающий - true',
-  country_id         INTEGER                              COMMENT 'Уникальный идентификатор страны',
-  document_id        INTEGER                              COMMENT 'Уникальный идентификатор документа',
-    version            INTEGER      NOT NULL                COMMENT 'Служебное поле hibernate',
-    
-
-  CONSTRAINT PK_EMPLOYEE_ID PRIMARY KEY (id)
-);
-
-
--- --------------------------------------------------------------------------------------------------
-
--- --------------------------------------------------------------------------------------------------
--- join-таблица для связи сотрудник - подразделение
-CREATE TABLE IF NOT EXISTS employee_office
-(
-  employee_id INTEGER COMMENT 'Уникальный идентификатор сотрудниа',
-  office_id   INTEGER COMMENT 'Уникальный идентификатор офиса',
-
-  CONSTRAINT PK_EMPLOYEE_OFFICE PRIMARY KEY (employee_id, office_id)
+CREATE TABLE IF NOT EXISTS country (
+   id      INTEGER     NOT NULL  PRIMARY KEY AUTO_INCREMENT  COMMENT 'Уникальный идентификатор',
+   name    VARCHAR(50) NOT NULL                              COMMENT 'Название страны',
+   version INTEGER     NOT NULL                              COMMENT 'Служебное поле hibernate',
+   code    INTEGER     NOT NULL                              COMMENT 'Уникальный код страны'
 );
 
 
 
 
-
-CREATE TABLE IF NOT EXISTS country
-(
-  id      INTEGER     NOT NULL AUTO_INCREMENT COMMENT 'Уникальный идентификатор',
-  code    VARCHAR(50) NOT NULL                COMMENT 'Цифровой код страны по ISO 3166-1',
-  name    VARCHAR(100)NOT NULL                COMMENT 'Название страны',
-  version INTEGER     NOT NULL                COMMENT 'Служебное поле hibernate',
-
-  CONSTRAINT PK_COUNTRIES_CATALG_ID PRIMARY KEY (id)
+CREATE TABLE IF NOT EXISTS document (
+   id      INTEGER     NOT NULL  PRIMARY KEY AUTO_INCREMENT  COMMENT 'Уникальный документ',
+   name    VARCHAR(50) NOT NULL                              COMMENT 'Название документа',
+   version INTEGER     NOT NULL                              COMMENT 'Служебное поле hibernate',
+   code    INTEGER     NOT NULL                              COMMENT 'Уникальный код документа'
 );
 
 
--- ------------------------------------------------------------------------------------------------------
--- справочник видов документов, удостоверяющих личность физического лица
-CREATE TABLE IF NOT EXISTS document
-(
-  id      INTEGER      NOT NULL AUTO_INCREMENT COMMENT 'Уникальный идентификатор',
-  code    VARCHAR(100) NOT NULL                COMMENT 'Код документа',
-  name    VARCHAR(100) NOT NULL                COMMENT 'Наименование документа',
-  version INTEGER      NOT NULL                COMMENT 'Служебное поле hibernate',
-    
 
-  CONSTRAINT PK_IDENTIFICATION_DOCUMENT_CATALOG_ID PRIMARY KEY (id)
+
+CREATE TABLE IF NOT EXISTS employee (
+   id             INTEGER     NOT NULL  PRIMARY KEY AUTO_INCREMENT  COMMENT 'Уникальный идентификатор',
+   first_name     VARCHAR(50) NOT NULL                              COMMENT 'Имя',
+   second_name    VARCHAR(50) NOT NULL                              COMMENT 'Фамилия',
+   middle_name    VARCHAR(50) NOT NULL                              COMMENT 'Отчество',
+   position       VARCHAR(50) NOT NULL                              COMMENT 'Должность',
+   phone          INTEGER     NOT NULL                              COMMENT 'Телефон',
+   is_identified  BOOLEAN                                           COMMENT 'Работающий или не работающий, если работающий - true',
+   country_id     INTEGER     NOT NULL                              COMMENT 'Уникальный идентификатор страны',
+   version       INTEGER     NOT NULL                              COMMENT 'Служебное поле hibernate',
+   document_id    INTEGER     NOT NULL                              COMMENT 'Уникальный идентификатор документа',
+   doc_name      INTEGER     NOT NULL,
+   doc_number     INTEGER     NOT NULL,
+   doc_data       date        NOT NULL
 );
+
+CREATE INDEX IX_employee_ibfk_1 ON employee (document_id);
+ALTER TABLE employee ADD FOREIGN KEY (document_id) REFERENCES document(id);
+
+CREATE INDEX IX_employee_ibfk_2 ON employee (country_id);
+ALTER TABLE employee ADD FOREIGN KEY (country_id) REFERENCES country(id);
+
+CREATE TABLE IF NOT EXISTS organization (
+  id             INTEGER     NOT NULL  PRIMARY KEY AUTO_INCREMENT    COMMENT 'Уникальный идентификатор',
+  name           VARCHAR(50) NOT NULL                                COMMENT 'Сокращенное название',
+  full_name      VARCHAR(50) NOT NULL                                COMMENT 'Полное название',
+  inn            INTEGER     NOT NULL                                COMMENT 'Идентификационный номер налогоплательщика',
+  kpp            INTEGER     NOT NULL                                COMMENT 'Код причины постановки на учет в налоговых органах',
+  address        VARCHAR(50) NOT NULL                                COMMENT 'Регистрационный адрес',
+  phone          INTEGER     NOT NULL                                COMMENT 'Номер телефона',
+  is_active      BOOLEAN                                             COMMENT 'Действующая организация, если действует - true',
+  version        INTEGER     NOT NULL                                COMMENT 'Служебное поле hibernate'
+);
+
+
+
+
+CREATE TABLE IF NOT EXISTS office (
+  id             INTEGER       NOT NULL  PRIMARY KEY AUTO_INCREMENT  COMMENT 'Уникальный идентификатор',
+  first_name     VARCHAR(50)   NOT NULL                              COMMENT 'Название',
+  address        VARCHAR(50)   NOT NULL                              COMMENT 'Регистрационный адрес',
+  phone          INTEGER       NOT NULL                              COMMENT 'Номер телефона',
+  is_active      BOOLEAN                                             COMMENT 'Действующий офис, если дейcтвует - true',
+  version        INTEGER       NOT NULL                              COMMENT 'Служебное поле hibernate',
+  org_id         INTEGER       NOT NULL
+);
+
+CREATE INDEX IX_office_ibfk_1 ON office (org_id);
+ALTER TABLE office ADD FOREIGN KEY (org_id) REFERENCES organization(id);
+
+
+
+CREATE TABLE IF NOT EXISTS Employee_Office (
+   employee_id    INTEGER     NOT NULL      COMMENT 'Уникальный идентификатор сотрудника',
+   office_id      INTEGER     NOT NULL      COMMENT 'Уникальный идентификатор офиса'
+);
+
+
+CREATE INDEX IX_employee_office_ibfk_1 ON Employee_Office (employee_id);
+ALTER TABLE Employee_Office ADD FOREIGN KEY (employee_id) REFERENCES employee(id);
+
+CREATE INDEX IX_employee_office_ibfk_2 ON Employee_Office(office_id);
+ALTER TABLE Employee_Office ADD FOREIGN KEY (office_id) REFERENCES office(id);
+
+
 
 
