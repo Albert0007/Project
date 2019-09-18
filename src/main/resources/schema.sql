86330318468f8b9f6058e1a070fb082c8bc1c815
Albert0007 <54860302+Albert0007@users.noreply.github.com>
2019-09-18 17:07:05 +0400

    Update schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS country (
    version INTEGER     NOT NULL                              COMMENT 'Служебное поле hibernate',
    code    INTEGER     NOT NULL                              COMMENT 'Уникальный код страны'
 );
-COMMENT ON TABLE Person IS 'Страна';
+COMMENT ON TABLE country IS 'Страна';
 
 CREATE TABLE IF NOT EXISTS document (
    id      INTEGER     NOT NULL  PRIMARY KEY AUTO_INCREMENT  COMMENT 'Уникальный документ',
@@ -13,9 +13,10 @@ CREATE TABLE IF NOT EXISTS document (
    version INTEGER     NOT NULL                              COMMENT 'Служебное поле hibernate',
    code    INTEGER     NOT NULL                              COMMENT 'Уникальный код документа'
 );
-COMMENT ON TABLE Person IS 'Документ';
+COMMENT ON TABLE document IS 'Документ';
 
 CREATE INDEX IX_document_ibfk_1 ON document(name);
+CREATE INDEX IX_document_ibfk_2 ON document(id);
 
 CREATE TABLE IF NOT EXISTS document_registry (
    id          INTEGER       NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'Уникальный идентификатор документа',
@@ -25,7 +26,7 @@ CREATE TABLE IF NOT EXISTS document_registry (
    version INTEGER           NOT NULL                            COMMENT 'Служебное поле hibernate'
   
 );
-COMMENT ON TABLE Person IS 'Документ_реестр';
+COMMENT ON TABLE document_registry IS 'Документ_реестр';
 
 CREATE INDEX IX_document_registry_ibfk_1 ON document_registry(doc_name);
 ALTER TABLE document_registry ADD FOREIGN KEY (doc_name) REFERENCES document(name);
@@ -44,7 +45,10 @@ CREATE TABLE IF NOT EXISTS employee (
    country_id     INTEGER     NOT NULL                              COMMENT 'Уникальный идентификатор страны',
    version        INTEGER     NOT NULL                              COMMENT 'Служебное поле hibernate'
 );
-COMMENT ON TABLE Person IS 'Сотрудник';
+COMMENT ON TABLE employee IS 'Сотрудник';
+
+CREATE INDEX IX_employee_ibfk_1 ON employee (document_id);
+ALTER TABLE employee ADD FOREIGN KEY (document_id) REFERENCES document(id);
 
 CREATE INDEX IX_employee_ibfk_2 ON employee (country_id);
 ALTER TABLE employee ADD FOREIGN KEY (country_id) REFERENCES country(id);
@@ -61,7 +65,7 @@ CREATE TABLE IF NOT EXISTS organization (
   is_active      BOOLEAN                                             COMMENT 'Действующая организация, если действует - true',
   version        INTEGER     NOT NULL                                COMMENT 'Служебное поле hibernate'
 );
-COMMENT ON TABLE Person IS 'Организация';
+COMMENT ON TABLE organization IS 'Организация';
 
 
 CREATE TABLE IF NOT EXISTS office (
@@ -73,7 +77,7 @@ CREATE TABLE IF NOT EXISTS office (
   version        INTEGER       NOT NULL                              COMMENT 'Служебное поле hibernate',
   org_id         INTEGER       NOT NULL
 );
-COMMENT ON TABLE Person IS 'Офис';
+COMMENT ON TABLE office IS 'Офис';
 
 CREATE INDEX IX_office_ibfk_1 ON office (org_id);
 ALTER TABLE office ADD FOREIGN KEY (org_id) REFERENCES organization(id);
@@ -84,13 +88,28 @@ CREATE TABLE IF NOT EXISTS Employee_Office (
    employee_id    INTEGER     NOT NULL      COMMENT 'Уникальный идентификатор сотрудника',
    office_id      INTEGER     NOT NULL      COMMENT 'Уникальный идентификатор офиса'
 );
-COMMENT ON TABLE Person IS 'join-таблица для связи сотрудника и офиса';
+COMMENT ON TABLE Employee_Office IS 'join-таблица для связи сотрудника и офиса';
 
 CREATE INDEX IX_employee_office_ibfk_1 ON Employee_Office (employee_id);
 ALTER TABLE Employee_Office ADD FOREIGN KEY (employee_id) REFERENCES employee(id);
 
 CREATE INDEX IX_employee_office_ibfk_2 ON Employee_Office(office_id);
 ALTER TABLE Employee_Office ADD FOREIGN KEY (office_id) REFERENCES office(id);
+
+
+CREATE TABLE IF NOT EXISTS Office_Organization (
+   org_id    INTEGER     NOT NULL      COMMENT 'Уникальный идентификатор организации',
+   off_id    INTEGER     NOT NULL      COMMENT 'Уникальный идентификатор офиса'
+);
+COMMENT ON TABLE Office_Organization IS 'join-таблица для связи офиса и организации';
+
+CREATE INDEX IX_office_organization_ibfk_1 ON Office_Organization (org_id);
+ALTER TABLE Office_Organization ADD FOREIGN KEY (org_id) REFERENCES organization(id);
+
+CREATE INDEX IX_office_organization_ibfk_2 ON Office_Organization(off_id);
+ALTER TABLE Office_Organization ADD FOREIGN KEY (off_id) REFERENCES office(id);
+
+
 
 
 
